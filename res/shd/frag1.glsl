282369ba7ba47eaeae6567632546ec6b4c6825b0
Jere Koivisto <jere.koivisto@outlook.com>
2020-01-15 15:42:33 +0200

    Add files via upload

--- res/shd/frag1.glsl
+++ res/shd/frag1.glsl
@@ -19,11 +19,18 @@ void main()
         uv.x += sin(elapsedTime * 4 + uv.y * 8) * 0.05;
         uv.y += cos(elapsedTime * 3 + uv.x * 0) * 0.05;
         color = 1 - texture(Texture, vec3(uv, texId));
+        //vec4 texColor = texture(Texture, vec3(uv, texId));
+        //color = vec4( 1, 1, 1, texColor.w);
     }
 
     else if(effect == 2)//Color changing effect
     {
-        color = texture(Texture, vec3(uv, texId)) * vec4(0.5f + 0.5f * cos(elapsedTime*2), 0.5f + 0.5f * sin(elapsedTime*2), 1.0f, 1.0f);
+        color = texture(Texture, vec3(uv, texId)) * vec4(0.5f + 0.5f * sin(elapsedTime*2 + 0), 0.5f + 0.5f * sin(elapsedTime*2 + 2 * 3.14 / 3), 0.5f + 0.5f * sin(elapsedTime*2 + 4 * 3.14 / 3), 1.0f);
+    }
+
+    else if(effect == 3)//Button highlight
+    {
+        color = texture(Texture, vec3(uv, texId)) + vec4(1.0f, 1.0f, 0.0f, 0.0f);
     }
 
     else//Normal
